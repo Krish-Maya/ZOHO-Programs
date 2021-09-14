b677d0ec3fc0e7b1dc0eaf685fe462a3f76d617d
Mayakrishnan M <79070296+Krish-Maya@users.noreply.github.com>
2021-09-14 16:50:15 +0530

    Update strrev.java
    push

--- strrev.java
+++ strrev.java
@@ -2,7 +2,7 @@ class strrev
 {
   public static void main( String[] args)
   {
-    String[] str={ "maya","krish","M"};
+    String[] str={ "maya","krish","M","mk"};
 	
 	for(int i=str.length-1;i>=0;i--)
 	{
